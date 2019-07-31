7568c171d52d8f8aa47fcb88802b8b2bab56e575
G <jay122493@gmail.com>
2019-07-31 15:23:26 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'typeface-roboto';
+import Profile from './components/login-page/Login-page.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
