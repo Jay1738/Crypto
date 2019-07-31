23927e00adbdd816b127f06437b58225f89b2b27
G <jay122493@gmail.com>
2019-07-31 15:24:39 -0400

    Update Topnav.js

---
+++ src/components/dashboard/topnav/Topnav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class Topnav extends Component {
+
+    render() {
+        return (
+            <div className="Topnav">
+
+            </div>
+        );
+    }
+}
