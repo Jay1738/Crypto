e7bed2b6710374d88920e6c142642888a6b79059
G <jay122493@gmail.com>
2019-07-31 16:31:06 -0400

    redo

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import 'typeface-roboto';
-import Login from './components/login-page/Login-page.js';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
